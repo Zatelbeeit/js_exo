2c91d6a6a28c2ae14e1b8e4d8cf0fb41dea76bb5
Zatelbeeit <breaul@gmail.com>
2019-09-23 13:34:39 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var smiley = [";-)", ";-)", ":-(", "X-D"];
+var replaceSmiley = ["---"];
+var dirty = document.getElementById("text");
+
+
+document.getElementById("moderate").addEventListener("click", function(){
+  document.getElementById("text").value;
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
